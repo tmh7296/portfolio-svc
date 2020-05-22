3c27fe24914ee84a524f700a88c2f1cb00e4fea1
Tim <tmh7296@g.rit.edu>
2020-05-22 16:07:27 -0400

    initial commit

---
+++ src/main/java/com/tmh/portfoliosvc/PortfolioSvcApplication.java
@@ -0,0 +1,13 @@
+package com.tmh.portfoliosvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioSvcApplication.class, args);
+	}
+
+}
